4e8ff0b6f70cdfe52b2d9c579aef58fe439f77ca
darshan <darshpat01@gmail.com>
2023-11-30 17:16:54 +0530

    [Test] - analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <Analytics />
+    <App />
+  </>
+);
