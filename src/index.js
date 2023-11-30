211cad1947373fb663af7e1e855054b23d8fbbac
darshan <darshpat01@gmail.com>
2023-11-30 18:11:11 +0530

    [Fix] - cleared analytics

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <Analytics />
-    <App />
-  </>
-);
+root.render(<App />);
